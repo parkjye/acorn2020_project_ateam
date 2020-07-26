f30bfc5f6ef1271174f1902dc43d3d78adf485ce
parkjye <parkjye44@gmail.com>
2020-07-26 23:20:36 +0900

    <추가>: <캘린더>
    TOAST UI를 활용한 캘린더 구현 중 입니다.

--- acorn_ateam/WebContent/assets/js/main.js
+++ acorn_ateam/WebContent/assets/js/main.js
@@ -12,63 +12,56 @@ function scrollTo(element, dir) {
   });
 }
 
+//윈도우 load 순서: html -> DOMContentLoaded -> css
 function init() {
   window.addEventListener("DOMContentLoaded", () => {
     scrollTo(document.getElementsByTagName("body")[0], "up");
   });
 }
 
-const roomTitles = ["1.강의실", "2.강의실", "3.강의실", "4.강의실"];
-const roomInfos = ["좋은 구조/좌석30개", "채광이 좋은 구조/좌석30개", "공간이 넓은 구조/좌석30개", "풍경이 좋은 구조/좌석30개"];
-const roomFloors = ["카페형", "사무실형", "도서관형", "야외형"];
-const roomCapacity = ["30명", "25명", "35명", "99명"];
-const roomSize = ["51평형", "48평형", "60평형", "55평형"];
-const roomImges = ["room1", "room2", "room3", "room4"];
-let roomInfoCount = 0;
-const roomInfoMaxCount = 4;
-function roomInfo() {
-  //
-  const content = document.querySelector("#roomCard_content");
+//tui.calendar는 인터넷에서 다운받은 script. tui.Calendar 객체를 생성해서 사용한다.
+var calendar = new Calendar('#calendar', {
+	  defaultView: 'month',
+	  taskView: true,
+	  template: {
+	    monthDayname: function(dayname) {
+	      return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+	    }
+	    
+	  }
+	});
 
-  const leftBtn = document.querySelector("#roomInfo__btn-left");
-  const rightBtn = document.querySelector("#roomInfo__btn-right");
-  const title = document.querySelector("#roomInfo__title");
-  const info = document.querySelector("#roomInfo__info");
-  const floor = document.querySelector("#roomInfo__floor");
-  const capacity = document.querySelector("#roomInfo__Capacity");
-  const size = document.querySelector("#roomInfo__Size");
-  const img = document.querySelector("#roomInfo__img");
-
-  leftBtn.addEventListener("click", () => {
-    roomInfoCount = (roomInfoCount - 1) % roomInfoMaxCount;
-    if (roomInfoCount < 0) roomInfoCount = 3;
-    changeElement(roomInfoCount);
-  });
-  rightBtn.addEventListener("click", () => {
-    roomInfoCount = (roomInfoCount + 1) % roomInfoMaxCount;
-    changeElement(roomInfoCount);
-  });
-
-  const changeElement = function (roomInfoCount) {
-    if (content.classList.contains("animate__animated")) {
-      content.classList.remove("animate__animated", "animate__zoomIn");
-      img.classList.remove("animate__animated", "animate__zoomIn");
+calendar.createSchedules([
+    {
+        id: '1',
+        calendarId: '1',
+        title: 'my schedule',
+        category: 'time',
+        dueDateClass: '',
+        start: '2018-01-18T22:30:00+09:00',
+        end: '2018-01-19T02:30:00+09:00'
+    },
+    {
+        id: '2',
+        calendarId: '1',
+        title: 'second schedule',
+        category: 'time',
+        dueDateClass: '',
+        start: '2018-01-18T17:30:00+09:00',
+        end: '2018-01-19T17:31:00+09:00',
+        isReadOnly: true //읽기전용
     }
+]);
 
-    setTimeout(animate, 16);
+calendar.updateSchedule(schedule.id, schedule.calendarId, {
+    start: startTime,
+    end: endTime
+});
 
-    function animate() {
-      title.innerText = roomTitles[roomInfoCount];
-      info.innerText = roomInfos[roomInfoCount];
-      floor.innerText = roomFloors[roomInfoCount];
-      capacity.innerText = roomCapacity[roomInfoCount];
-      size.innerText = roomCapacity[roomInfoCount];
-      img.src = `./assets/images/${roomImges[roomInfoCount]}.jpg`;
-      content.classList.add("animate__animated", "animate__zoomIn");
-      img.classList.add("animate__animated", "animate__zoomIn");
-    }
-  };
-}
+/*달력 읽기전용
+calendar.createSchedule({
+    title: 'read-only schedule',
+    isReadOnly: true
+});*/
 
 init();
-roomInfo();
--- acorn_ateam/WebContent/pages/reserve-calender.jsp
+++ acorn_ateam/WebContent/pages/reserve-calender.jsp
@@ -1,12 +1,53 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%
+	//DB에서 json이든 배열형태든 어떤 데이터로 받아오겠지요?
+	//배열로 뭔가 생겨있을거아냐 ?
+%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+
+<!-- js, css파일을 클라우드에서 다운로드하는 방식  -->
+<link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui-calendar/latest/tui-calendar.css" />
+<link rel="stylesheet" href="https://nhn.github.io/tui.calendar/latest/examples/css/icons.css">
+
+<!-- If you use the default popups, use this. -->
+<link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui.date-picker/latest/tui-date-picker.css" />
+<link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui.time-picker/latest/tui-time-picker.css" />
+<script src="https://uicdn.toast.com/tui.code-snippet/v1.5.2/tui-code-snippet.min.js"></script>
+<script src="https://uicdn.toast.com/tui.time-picker/latest/tui-time-picker.min.js"></script>
+<script src="https://uicdn.toast.com/tui.date-picker/latest/tui-date-picker.min.js"></script>
+<script src="https://uicdn.toast.com/tui-calendar/latest/tui-calendar.js"></script>
+
+<script defer type="module"  src="${pageContext.request.contextPath}/assets/js/main.js?v=<%=System.currentTimeMillis() %>"></script>
+   
 </head>
 <body>
 <h1>예약달력이 나오는 페이지 입니다.</h1>
+<!-- 우측상단에 Today, 이전달, 다음달 버튼 div -->
+<div id="menu">
+	<span id="menu-navi">
+	    <button type="button" class="btn btn-default btn-sm move-today" data-action="move-today">Today</button>
+	    <button type="button" class="btn btn-default btn-sm move-day" data-action="move-prev">
+			<i class="calendar-icon ic-arrow-line-left" data-action="move-prev"></i>
+	    </button>
+	    <button type="button" class="btn btn-default btn-sm move-day" data-action="move-next">
+			<i class="calendar-icon ic-arrow-line-right" data-action="move-next"></i>
+		</button>
+	</span>
+	<span id="renderRange" class="render-range"></span>
+</div>
+
+<!-- 달력 height크기는 임시로 지정합니다. -->
+<div id="calendar" style="height: 500px;"></div>
+
+
+<script>
+
+</script>
+
 </body>
 </html>
