bd2f510eadfdf9dedcbc22f5f45069690ac885b0
parkjye <parkjye44@gmail.com>
2020-07-27 19:06:22 +0900

    <추가>: <DateDao 추가>
    getDataList(): 일정 전체 정보를 return 받는 메소드 추가합니다.
    getUsing(): 예약 가능한 방인지 확인 할 수 있는 메소드 추가합니다.

--- acorn_ateam/src/date/dao/DateDao.java
+++ acorn_ateam/src/date/dao/DateDao.java
@@ -3,6 +3,8 @@ package date.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import app.util.DbcpBean;
 import date.dto.DateDto;
@@ -17,8 +19,67 @@ public class DateDao {
 		return dao;
 	}
 	
+	//return 일정
+	public List<DateDto> getDateList() {
+		
+		List<DateDto> list = new ArrayList<>();
+		
+		//필요한 객체의 참조값을 담을 지역변수 만들기 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+
+			//실행할 sql 문 준비하기
+			String sql = "select date_num, date_year, date_month, date_day"
+					+ " date_isUsing, date_sale, date_holiday, room_name"
+					+ " from tb_date";
+
+			pstmt = conn.prepareStatement(sql);
+
+			//sql 문에 ? 에 바인딩 
+			
+			//select 문 수행하고 결과 받아오기 
+			rs = pstmt.executeQuery();
+
+			//반복문 돌면서 결과 값 추출하기 
+			while (rs.next()) {
+				DateDto tmp = new DateDto();
+				
+				tmp.setDate_num(rs.getInt("date_num"));
+				tmp.setDate_year(rs.getString("date_year"));
+				tmp.setDate_month(rs.getString("date_month"));
+				tmp.setDate_day(rs.getString("date_day"));
+				tmp.setDate_isUsing(rs.getInt("date_isUsing"));
+				tmp.setDate_sale(rs.getInt("date_sale"));
+				tmp.setDate_holiday(rs.getInt("date_holiday"));
+				tmp.setRoom_name(rs.getString("room_name"));
+				
+				list.add(tmp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
+
+	
+	
 	//예약가능 여부 확인 예약가능=1, 예약불가=0
-	public DateDto isUsing(DateDto dto) {
+	public DateDto getUsing(DateDto dto) {
 		
 		DateDto Dto = null;
 		
@@ -67,5 +128,8 @@ public class DateDao {
 		}
 		return Dto;
 	}
+	
+	
+	//
 	
 }//isUsing
